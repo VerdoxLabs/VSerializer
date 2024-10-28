2dc533c9fb415b37901e5fa544505772ff943928
verdox <mail.ysp@web.de>
2024-10-28 21:06:59 +0000

    Return new array utility functions.

--- src/main/java/de/verdox/vserializer/generic/SerializationArray.java
+++ src/main/java/de/verdox/vserializer/generic/SerializationArray.java
@@ -26,4 +26,170 @@ public interface SerializationArray extends SerializationElement, Iterable<Seria
     default boolean isArray() {
         return true;
     }
+
+
+
+    default boolean isBoolArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isBoolean())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isCharArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isCharacter())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isStringArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isString())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isByteArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isByte())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isShortArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isShort())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isIntArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isInteger())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isFloatArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isFloat())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isDoubleArray() {
+        for (SerializationElement element : this) {
+            if (!element.isPrimitive() || !element.getAsPrimitive().isDouble())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isArrayOfArrays() {
+        for (SerializationElement element : this) {
+            if (!element.isArray())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean isContainerArray() {
+        for (SerializationElement element : this) {
+            if (!element.isContainer())
+                return false;
+        }
+        return true;
+    }
+
+    default boolean[] getAsBooleanArray() {
+        boolean[] array = new boolean[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsBoolean();
+        }
+        return array;
+    }
+
+    default char[] getAsCharArray() {
+        char[] array = new char[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsCharacter();
+        }
+        return array;
+    }
+
+    default byte[] getAsByteArray() {
+        byte[] array = new byte[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsByte();
+        }
+        return array;
+    }
+
+    default short[] getAsShortArray() {
+        short[] array = new short[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsShort();
+        }
+        return array;
+    }
+
+    default int[] getAsIntArray() {
+        int[] array = new int[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsInt();
+        }
+        return array;
+    }
+
+    default long[] getAsLongArray() {
+        long[] array = new long[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsLong();
+        }
+        return array;
+    }
+
+    default float[] getAsFloatArray() {
+        float[] array = new float[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsFloat();
+        }
+        return array;
+    }
+
+    default double[] getAsDoubleArray() {
+        double[] array = new double[length()];
+        int counter = 0;
+        for (SerializationElement element : this) {
+            array[counter++] = element.getAsDouble();
+        }
+        return array;
+    }
+
+    default String[] getAsStringArray() {
+        return stream().map(SerializationElement::getAsString).toArray(String[]::new);
+    }
+
+    default SerializationArray[] getAsArrayOfArrays() {
+        return stream().map(SerializationElement::getAsArray).toArray(SerializationArray[]::new);
+    }
+
+    default SerializationContainer[] getAsContainerArray() {
+        return stream().map(SerializationElement::getAsContainer).toArray(SerializationContainer[]::new);
+    }
 }
