029576927949066390fba05fc4add203f5cab8a0
verdox <mail.ysp@web.de>
2024-10-28 20:04:59 +0000

    Adding javadoc to context() method in ContextBasedTest.java

--- src/test/java/test/ContextBasedTest.java
+++ src/test/java/test/ContextBasedTest.java
@@ -3,5 +3,9 @@ package test;
 import de.verdox.vserializer.generic.SerializationContext;
 
 public abstract class ContextBasedTest {
+    /**
+     * Creates a new {@link SerializationContext} object
+     * @return a new serialization context object
+     */
     public abstract SerializationContext context();
 }
