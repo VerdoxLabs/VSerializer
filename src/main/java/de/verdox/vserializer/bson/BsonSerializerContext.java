0b9a823958b144b14a00f46b8b8fa61d26a32b60
verdox <mail.ysp@web.de>
2025-05-14 12:54:55 +0000

    Add Bson serializer context

--- src/main/java/de/verdox/vserializer/bson/BsonSerializerContext.java
+++ src/main/java/de/verdox/vserializer/bson/BsonSerializerContext.java
@@ -1,12 +1,21 @@
 package de.verdox.vserializer.bson;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import de.verdox.vserializer.json.JsonSerializationContainer;
+import de.verdox.vserializer.json.JsonSerializationElement;
 import de.verdox.vserializer.json.JsonSerializerContext;
 
 public class BsonSerializerContext extends JsonSerializerContext {
     @Override
     public JsonSerializationContainer createContainer() {
         return new BsonSerializationContainer(this, new JsonObject());
+    }
+
+    @Override
+    public JsonSerializationElement toElement(JsonElement jsonElement) {
+        if (jsonElement.isJsonObject())
+            return new BsonSerializationContainer(this, jsonElement.getAsJsonObject());
+        return super.toElement(jsonElement);
     }
 }
