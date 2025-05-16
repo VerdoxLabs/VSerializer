9105c40a82046441f27165755042626d1505c0c1
verdox <mail.ysp@web.de>
2025-05-16 10:04:05 +0000

    Fix Bson serialization context

--- src/main/java/de/verdox/vserializer/bson/BsonSerializationContainer.java
+++ src/main/java/de/verdox/vserializer/bson/BsonSerializationContainer.java
@@ -1,20 +1,22 @@
 package de.verdox.vserializer.bson;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import de.verdox.vserializer.generic.SerializationElement;
 import de.verdox.vserializer.json.JsonSerializationContainer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.Set;
 
 public class BsonSerializationContainer extends JsonSerializationContainer {
     public BsonSerializationContainer(BsonSerializerContext serializerContext, JsonObject jsonElement) {
-        super(serializerContext, jsonElement);
+        super(serializerContext, convertKeys(jsonElement));
     }
 
     @Override
     public Collection<String> getChildKeys() {
-        return super.getChildKeys().stream().map(this::fromBsonConformKey).toList();
+        return super.getChildKeys().stream().map(BsonSerializationContainer::fromBsonConformKey).toList();
     }
 
     @Override
@@ -37,14 +39,25 @@ public class BsonSerializationContainer extends JsonSerializationContainer {
         super.remove(toBsonConformKey(key));
     }
 
-    private String toBsonConformKey(String key) {
+    public static JsonObject convertKeys(JsonObject jsonObject) {
+        for (String s : Set.copyOf(jsonObject.keySet())) {
+            JsonElement removed = jsonObject.remove(s);
+            if(removed.isJsonObject()) {
+                removed = convertKeys(removed.getAsJsonObject());
+            }
+            jsonObject.add(toBsonConformKey(s), removed);
+        }
+        return jsonObject;
+    }
+
+    public static String toBsonConformKey(String key) {
         if (key.startsWith("$")) {
             key = "___dollar___" + key.substring(1);
         }
         return key.replace(".", "___dot___");
     }
 
-    private String fromBsonConformKey(String key) {
+    public static String fromBsonConformKey(String key) {
         if (key.startsWith("___dollar___")) {
             key = "$" + key.substring("___dollar___".length());
         }
--- src/test/java/bson/BsonSerializerTests.java
+++ src/test/java/bson/BsonSerializerTests.java
@@ -1,8 +1,16 @@
 package bson;
 
+import com.google.gson.JsonObject;
+import de.verdox.vserializer.bson.BsonSerializationContainer;
 import de.verdox.vserializer.bson.BsonSerializerContext;
 import de.verdox.vserializer.generic.SerializationContext;
+import de.verdox.vserializer.json.JsonSerializationElement;
+import de.verdox.vserializer.util.gson.JsonObjectBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import test.SerializerTests;
+
+import java.io.IOException;
 
 public class BsonSerializerTests extends SerializerTests {
     private static final SerializationContext SERIALIZATION_CONTEXT = new BsonSerializerContext();
@@ -10,5 +18,25 @@ public class BsonSerializerTests extends SerializerTests {
     @Override
     public SerializationContext context() {
         return SERIALIZATION_CONTEXT;
+    }
+
+/*    @Test
+    public void testConversionOfInvalidKeys() throws IOException {
+
+        JsonObject jsonObject = JsonObjectBuilder.create()
+                .add("invalid.key", "invalid_value")
+                .add("$invalid.key", "invalid_value")
+                .add("invalid.key2", JsonObjectBuilder.create()
+                        .add("$invalid.key", "invalid_value"))
+                .build();
+
+        BsonSerializerContext bsonSerializerContext = (BsonSerializerContext) context();
+        JsonSerializationElement jsonSerializationElement = bsonSerializerContext.toElement(jsonObject);
+    }*/
+
+    @Test
+    public void testToBsonConformKey() {
+        String convertedKey = BsonSerializationContainer.toBsonConformKey("invalid.key");
+        Assertions.assertEquals("invalid___dot___key", convertedKey);
     }
 }
